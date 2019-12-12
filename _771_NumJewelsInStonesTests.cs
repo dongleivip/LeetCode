e1d3f0ff3c644dec12b29a96b7f54eeaf88b22e0
Lei <dongleivip@gmail.com>
2019-12-12 22:29:00 +0800

    v_771 simplify code to v2

--- _771_NumJewelsInStonesTests.cs
+++ _771_NumJewelsInStonesTests.cs
@@ -36,5 +36,33 @@ namespace LeetCode
 
             return jewels.Sum(j => stones.ContainsKey(j) ? stones[j] : 0);
         }
+
+        [Fact]
+        public void ShouldReturnThree_v2()
+        {
+            var J = "aA";
+            var S = "aAAbbbb";
+
+            var result = NumJewelsInStones_v2(J, S);
+            Assert.Equal(3, result);
+        }
+
+        [Fact]
+        public void ShouldReturnZero_v2()
+        {
+            var J = "z";
+            var S = "ZZ";
+
+            var result = NumJewelsInStones_v2(J, S);
+            Assert.Equal(0, result);
+        }
+
+        public int NumJewelsInStones_v2(string J, string S)
+        {
+            var jewels = J.ToCharArray();
+            var stones = S.ToCharArray();
+
+            return jewels.Sum(j => stones.Count(s => s == j));
+        }
     }
 }
